b29a3d54fa970b96baf9a8a8070081f70168af79
Alfred Qiu <sc941203@gmail.com>
2020-05-03 18:47:18 +0800

    mybatis-mapper 代码生成器

---
+++ src/main/java/com/example/demo/dao/mapper/MapperTestMapper.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao.mapper;
+
+import com.example.demo.dao.entity.MapperTest;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MapperTestMapper extends Mapper<MapperTest> {
+}
